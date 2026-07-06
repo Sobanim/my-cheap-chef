4ebe383f6d5d9afeba5f93cd1a9430c0c05e6b89
Sobanim <sobolev.dmirtiy@gmail.com>
2026-07-06 22:24:52 +0200

    Add posthog

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { ProductCard } from './ProductCard/ProductCard';
 export { ProductGrid } from './ProductGrid/ProductGrid';
 export { EmptyState } from './EmptyState/EmptyState';
 export { ProductCatalog } from './ProductCatalog/ProductCatalog';
+export { PostHogProvider } from './PostHogProvider/PostHogProvider';
 
